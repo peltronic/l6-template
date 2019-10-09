60650ed1027f312437b35c4dd44e574fc27afadc
Peter Gorgone <peter@peltronic.com>
2019-10-09 16:38:13 -0700

    working subset test

--- tests/Feature/AccountsListTest.php
+++ tests/Feature/AccountsListTest.php
@@ -3,19 +3,21 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\TestResponse;
 use Tests\TestCase;
 
 use App\Models\Account;
 use App\Models\Listing;
 
+use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
+
 class AccountsListTest extends TestCase
 {
-
-    use RefreshDatabase;
+    use RefreshDatabase, ArraySubsetAsserts;
 
     public function testResponseStructure()
     {
-        factory(Account::class, 5)->make()->each(function (Account $account) {
+        $accounts = factory(Account::class, 5)->make()->each(function (Account $account) {
             $account->save();
             $account->listings()->saveMany([
                 factory(Listing::class)->create(),
@@ -26,8 +28,8 @@ class AccountsListTest extends TestCase
         //$response = $this->actingAs($user,'web')->ajaxJSON('POST',$url,$payload);
         $response = $this->ajaxJSON('GET','/api/accounts',[]);
 
-$content = json_decode($response->content());
-//dd($content);
+        //$content = json_decode($response->content());
+        //dd($content);
 
         //dd($response->getContent());
         $response->assertStatus(200);
@@ -63,6 +65,91 @@ $content = json_decode($response->content());
             ],
         ]);
     }
+
+    public function testListWithoutFilters()
+    {
+        $accounts = factory(Account::class, 5)->make()->each(function (Account $account) {
+            $account->save();
+            $account->listings()->saveMany([
+                factory(Listing::class)->create(),
+            ]);
+        });
+        //dd($accounts);
+
+        $response = $this->ajaxJSON('GET','/api/accounts',[]);
+
+        $response->assertStatus(200);
+        $this->assertResponseContainsAccounts($response, ...$accounts);
+    }
+
+    public function testNameFilter()
+    {
+        $account1 = factory(Account::class)->create(['aname' => 'PHP for begginers']);
+        $account2 = factory(Account::class)->create(['aname' => 'Javascript for dummies']);
+        $account3 = factory(Account::class)->create(['aname' => 'Advanced Python']);
+
+        /*
+        $account1->aname = 'blah';
+        $account1->save();
+        $account2->aname = 'blah';
+        $account2->save();
+        //dd($account2);
+         */
+
+        $response = $this->ajaxJSON('GET','/api/accounts',['filters'=>['aname'=>'nothere']]); // %FIXME: should fail
+        $response->assertStatus(200);
+        $content = json_decode($response->content(),true);
+
+        $a = $content['data'];
+        $s = [$this->accountToResourceArray($account1)];
+        //dd( $a );
+        //dd( $s );
+        //$this->assertArraySubset( $s, $a );
+        self::assertArraySubset( $s, $a );
+
+        //$this->assertResponseContainsAccounts($response, $account1);
+        /*
+        dd( array_map(function (Account $a) {
+            return $this->accountToResourceArray($a);
+        }, [$account1]) );
+
+        array:1 [
+            0 => array:4 [
+                "id" => 1
+                "aname" => "blah"
+                "adesc" => "Totam molestiae unde sit qui qui."
+                "listings" => []
+            ]
+        ]
+         */
+        /*
+        $response->assertJson([
+            'data' => array_map(function (Account $a) {
+                return $this->accountToResourceArray($a);
+            }, [$account1]),
+        ]);
+         */
+
+        /*
+        $response = $this->ajaxJSON('GET','/api/accounts',['filters'=>['aname'=>'northere']]); // %FIXME: should fail
+        $response->assertStatus(200);
+        $this->assertResponseContainsAccounts($response, $account1, $account2);
+         */
+    }
+
+    // ---------------------------------------------
+
+
+    private function assertResponseContainsAccounts(TestResponse $response, ...$accounts): void
+    {
+        $response->assertJson([
+            'data' => array_map(function (Account $account) {
+                return $this->accountToResourceArray($account);
+            }, $accounts),
+        ]);
+        // The assertJson method converts the response to an array and utilizes PHPUnit::assertArraySubset to verify that the given array exists within the JSON response returned by the application. So, if there are other properties in the JSON response, this test will still pass as long as the given fragment is present.
+    }
+
 
     /*
     public function testExample()
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,9 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Models\Account;
+use App\Models\Listing;
+
 
 abstract class TestCase extends BaseTestCase
 {
@@ -10,5 +13,30 @@ abstract class TestCase extends BaseTestCase
     // see: vendor//laravel/framework/src/Illuminate/Foundation/Testing/Concerns/MakesHttpRequests.php
     public function ajaxJSON($method,$uri, array $data=[]) {
         return $this->json($method,$uri,$data,['HTTP_X-Requested-With'=>'XMLHttpRequest']);
+    }
+
+    protected function accountToResourceArray(Account $obj)
+    {
+        return [
+            'id' => $obj->id,
+            'aname' => $obj->aname,
+            'adesc' => $obj->adesc,
+            'listings' => [],
+        ];
+        /*
+        return [
+            'id' => $account->id,
+            'isbn' => $account->isbn,
+            'title' => $account->title,
+            'description' => $account->description,
+            'authors' => $account->authors->map(function (Author $author) {
+                return ['id' => $author->id, 'name' => $author->name, 'surname' => $author->surname];
+            })->toArray(),
+            'review' => [
+                'avg' => (int) round($account->reviews->avg('review')),
+                'count' => (int) $account->reviews->count(),
+            ],
+        ];
+         */
     }
 }
